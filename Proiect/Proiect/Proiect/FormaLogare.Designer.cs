7d3ad3f31bc9c5d86079450ab83dd3afbcded27e
Mihai Barbu <mihaibarbu@outlook.com>
2017-06-06 11:21:38 +0300

    login v2

--- Proiect/Proiect/Proiect/FormaLogare.Designer.cs
+++ Proiect/Proiect/Proiect/FormaLogare.Designer.cs
@@ -34,13 +34,15 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 30F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(117, 46);
+            this.label1.Location = new System.Drawing.Point(124, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(153, 54);
             this.label1.TabIndex = 999;
@@ -49,7 +51,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.textBox1.Location = new System.Drawing.Point(12, 152);
+            this.textBox1.Location = new System.Drawing.Point(12, 149);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(360, 29);
             this.textBox1.TabIndex = 0;
@@ -58,7 +60,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.textBox2.Location = new System.Drawing.Point(12, 187);
+            this.textBox2.Location = new System.Drawing.Point(12, 184);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(360, 29);
@@ -68,7 +70,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Location = new System.Drawing.Point(12, 225);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 39);
             this.button1.TabIndex = 1000;
@@ -79,7 +81,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(195, 226);
+            this.button2.Location = new System.Drawing.Point(195, 225);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(177, 39);
             this.button2.TabIndex = 1001;
@@ -87,11 +89,35 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(195, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 39);
+            this.button3.TabIndex = 1002;
+            this.button3.Text = "Adauga";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(163, 100);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(69, 13);
+            this.linkLabel1.TabIndex = 1003;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Adauga Cont";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
             // FormaLogare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 277);
+            this.ClientSize = new System.Drawing.Size(384, 275);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -116,5 +142,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- Proiect/Proiect/Proiect/FormaLogare.cs
+++ Proiect/Proiect/Proiect/FormaLogare.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Proiect
 {
+
     public partial class FormaLogare : Form
     {
+        string admin;
+        SqlConnection conect = new SqlConnection(@"Data Source=pacienti.database.windows.net;Initial Catalog=database;Integrated Security=False;User ID=" + Properties.Resources.Cont.ToString() + ";Password=" + Properties.Resources.Parola.ToString() + ";Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
         public FormaLogare()
         {
@@ -20,7 +25,8 @@ namespace Proiect
 
         private void FormaLogare_Load(object sender, EventArgs e)
         {
-            BackColor= System.Drawing.ColorTranslator.FromHtml("#F7F5E6");
+            BackColor = System.Drawing.ColorTranslator.FromHtml("#F7F5E6");
+            button3.Hide();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,9 +36,80 @@ namespace Proiect
 
         private void button2_Click(object sender, EventArgs e)
         {
+            if (NetworkInterface.GetIsNetworkAvailable() == false)
+            {
+                MessageBox.Show("Lipsa internet !", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+            else
+            {
+                string parola = textBox2.Text;
+                try
+                {
+                    SqlCommand comanda = new SqlCommand("select parola from dbo.useri where nume ='" + textBox1.Text + "'", new SqlConnection(conect.ConnectionString));
+                    comanda.Connection.Open();
+                    if (parola == comanda.ExecuteScalar().ToString())
+                    {
+                        SqlCommand comanda2 = new SqlCommand("select admin from dbo.useri where nume ='" + textBox1.Text + "'", new SqlConnection(conect.ConnectionString));
+                        comanda2.Connection.Open();
+                        admin = comanda2.ExecuteScalar().ToString();
+                        comanda2.Connection.Close();
+                        MessageBox.Show(admin.ToString(), "sad");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Date introduse gresit !", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    comanda.Connection.Close();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            refreshtext();
+
             Form1 formaintrare = new Form1();
             formaintrare.Show();
-            Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (NetworkInterface.GetIsNetworkAvailable() == false)
+            {
+                MessageBox.Show("Lipsa internet !", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    SqlCommand comanda = new SqlCommand("insert into dbo.useri values('" + textBox1.Text + "','" + textBox2.Text + "', '0')", new SqlConnection(conect.ConnectionString));
+                    comanda.Connection.Open();
+                    comanda.ExecuteNonQuery();
+                    MessageBox.Show("Cont Introdus !", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    comanda.Connection.Close();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                button2.Show();
+                button3.Hide();
+                refreshtext();
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            button3.Show();
+            button2.Hide();
+            refreshtext();
+        }
+
+        private void refreshtext()
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
         }
     }
 }
